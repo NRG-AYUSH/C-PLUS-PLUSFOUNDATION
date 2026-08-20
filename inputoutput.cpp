34c2456fdb36f5cdbe39c690a19b51fe93c9d011
NRG-AYUSH <noobk8523@gmail.com>
2026-08-20 20:30:11 +0530

    Learning about the Input and Output

---
+++ inputoutput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main()
+{
+    int a,b,sum;
+    std::cout<<"Welcome to the Code\n";
+    std::cout<<"Enter the first Number\n";
+    std::cin>>a;
+    std::cout<<"Enter the Second number\n";
+    std::cin>>b;
+    sum=a+b;
+    std::cout<<"The sum Of the numbers is\n";
+    std::cout<<sum;
+return 0;
+}
