1b9498ef5b0481a10411ce251057a04e7f0106f6
NRG-AYUSH <noobk8523@gmail.com>
2026-08-20 20:19:32 +0530

    Learning about Variables

---
+++ Variables.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main()
+{
+int a=10;
+double aa =1000.2934;
+double sum = a+aa;
+std ::cout <<"hello welcome to the code,The sum of the numbers        "<<a<<"and"<<aa<<"is\n"<<sum;
+return 0;
+}
