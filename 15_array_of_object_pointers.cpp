6d68b8bd29378ce8e44afbc4c42eaa2f059b959f
NRG-AYUSH <noobk8523@gmail.com>
2026-09-09 16:32:32 +0530

    .

---
+++ 15_array_of_object_pointers.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Employee
+{
+private:
+    int id;
+    double salary;
+    string name;
+
+public:
+    Employee(int i, double s, string n) : id(i), salary(s), name(n) {}
+    double getSalary()
+    {
+        return salary;
+    }
+    void infoDisplay()
+    {
+        cout << "-----EMPLOYEE DETAILS-------" << endl;
+        cout << "Name: " << name << "\nID: " << id << "\nSalary: " << salary << endl;
+    }
+};
+int main()
+{
+
+    Employee *team[3];
+    team[0] = new Employee(1205, 100000.02, "AYUSH");
+    team[1] = new Employee(207, 100000.02, "SNEHA");
+    team[2] = new Employee(154, 10000.02, "HEHE");
+    team[3] = new Employee(123, 10000.02, "HURRR");
+    cout << "=== Employees with Salary > 50,000 ===" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        // Arrow operator and getter used
+        if (team[i]->getSalary() > 50000)
+        {
+            team[i]->infoDisplay();
+        }
+    }
+    for (int j = 0; j < 3; j++)
+    {
+        delete team[j];
+        team[j] = nullptr;
+    }
+
+    return 0;
+}
